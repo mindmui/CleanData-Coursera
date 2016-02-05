ea79c62b927257a30c98f0a6c53c1345f6951408
MindMui <Mind@Mind-MacBook-Pro.local>
2016-02-06 01:24:56 +0800

    Update

--- week1.R
+++ week1.R
@@ -35,6 +35,35 @@
     dateDownloaded <- date() # a good practice to show which day you downlaoded the file
     # Remarks: if the url is 'https', on a Mac, you need to set the method = "curl"
     
-  # Reading local flat files
-    
+# Reading local flat files
+  # Loading flat files - read.table()
+    # main funtion for reading data into R
+    # Reads the data into RAM, big data can cause problem
+      cameraData <- read.table("./data/filename.csv", sep=",", header = TRUE)
+      # if the data contains the columns names
+      # alternatively, use read.csv()
+      # More important parameters:
+        # na.strings - set the character that represents missing value (e.g. 999)
+        # skip - number of lines to skip before starting to read
+        # nrows - how many rows to read of the file
+        # quote = "" often resolves ` or " issues
+
+# Reading Excel files
+  # Most widely used format ---> spreadsheets
+  # requires xlsx package **
+  library(xlsx)
+  cameraData  <- read.xlsx("./file.xlsx",sheetIndex = 1,header=TRUE)
+  # you can read specific rows and columns by passing colIndex and rowIndex parameters
+    # (reading subset of the excel file)
+  # Further notes:
+    # read.xlsx2 is much faster
+    # write.xlsx will write out
+    # in general, it is advised to store your data in .csv as they are easier to distribute
+  
+# Reading XML 
+  # extensible markup language
+  # frequently used to store structured data
+  # Extracting XML is the basis for most web scraping
+  
+        
     
